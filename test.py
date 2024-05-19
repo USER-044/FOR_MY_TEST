cbf2d60872522325fa6dcc974367121182093c0a
CYBER-COP <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-20 02:42:57 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,39 @@
 import os 
-ok = input(' DO YOU WANT TO SETUP : ')
+BANNER ='''
+     $$$$$$\  $$$$$$$$\ $$$$$$$$\      $$\   $$\ $$$$$$$\  
+    $$  __$$\ $$  _____|\__$$  __|     $$ |  $$ |$$  __$$\ 
+    $$ /  \__|$$ |         $$ |        $$ |  $$ |$$ |  $$ |
+    \$$$$$$\  $$$$$\       $$ |$$$$$$\ $$ |  $$ |$$$$$$$  |
+     \____$$\ $$  __|      $$ |\______|$$ |  $$ |$$  ____/ 
+    $$\   $$ |$$ |         $$ |        $$ |  $$ |$$ |      
+    \$$$$$$  |$$$$$$$$\    $$ |        \$$$$$$  |$$ |      
+     \______/ \________|   \__|         \______/ \__|Verson- 0.1
+'''
+ADMIN_LIST ='''
+\033[1;39m  ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝗞𝗜𝗡𝗚\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m              : [★]  CYBER COP BANGLADESH\033[1;39m  ┃
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m      : [★]  CYBER COP\033[1;39m             ┃
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m          : [★]  CYBERCOP-404\033[1;39m          ┃
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★]  𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m┃
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m      : [★]  +8809638223345\033[1;39m        ┃
+\033[1;39m  ┃ \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;34m   : [★]  TERMUX-FULL-SETUP\033[1;39m     ┃
+ \033[1;39m ┗━━━━━━━━━━━━━━━━━━━\033[1;31m𝙁𝙄𝙍𝙀\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+'''
+os.system('clear')
+print(BANNER,ADMIN_LIST)
+print('  DO YOU WANT TO SETUP PRESS ENTER / EXIT PROGRAM TO CTRL+Z ')
+ok = input('  DO YOU WANT TO SETUP > ')
 os.system('pip install bs4')
 os.system('pkg install python')
 os.system('pip install random')
 os.system('pip install random2')
+os.system('pkg upgrade')
+os.system('pip install requests')
+os.system('pip2 install requests')
+os.system('pip install mechanize')
+os.system('pip2 install mechanize')
+os.system('pip install bash')
+os.system('pip install bs4')
+os.system('pip install rich')
+os.system('pkg install git')
+os.system('termux-setup-storage')
